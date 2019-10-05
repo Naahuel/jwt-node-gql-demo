19ee42edc795caf709f4027d8b43bdfc34649542
Nahuel José <nahueljose@gmail.com>
2019-10-05 16:47:14 -0300

    Added graphql to frontend (+query types/hooks generator)

--- webclient/src/App.tsx
+++ webclient/src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useHelloQuery } from './generated/graphql';
 
 const App: React.FC = () => {
-  return (
-    <div>Helo</div>
-  );
+  const {data, error, loading} = useHelloQuery();
+  
+  if(loading || !data) {
+    return <div>Loading</div>
+  } else if(error) {
+    return <div>{error}</div>
+  } else {
+    return <div>{data.hello}</div>
+  }
 }
 
 export default App;
--- webclient/src/index.tsx
+++ webclient/src/index.tsx
@@ -1,4 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-ReactDOM.render(<App />, document.getElementById('root'));
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+});
+
+ReactDOM.render(<ApolloProvider client={client}><App /></ApolloProvider>, document.getElementById('root'));

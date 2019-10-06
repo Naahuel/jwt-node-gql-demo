3f530c6d8eb57922ef9fed02ef0301016b55c604
Nahuel José <nahueljose@gmail.com>
2019-10-06 17:33:49 -0300

    Start login

--- webclient/src/pages/Login.tsx
+++ webclient/src/pages/Login.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { useLoginMutation } from "../generated/graphql";
+import { RouteComponentProps } from "react-router";
 
-interface Props {}
-
-export const Login: React.FC<Props> = () => {
-  return <div>login page</div>;
+export const Login: React.FC<RouteComponentProps> = ({ history }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [login] = useLoginMutation();
+  return (
+    <form
+      onSubmit={async e => {
+        e.preventDefault();
+        const response = await login({ variables: { email, password } });
+        console.log(response.data!.login.accessToken);
+      }}
+    >
+      <div>
+        <input
+          placeholder="Email"
+          value={email}
+          onChange={e => {
+            setEmail(e.target.value);
+          }}
+        />
+      </div>
+      <div>
+        <input
+          placeholder="Password"
+          type="password"
+          value={password}
+          onChange={e => {
+            setPassword(e.target.value);
+          }}
+        />
+      </div>
+      <div>
+        <input type="submit" value="Login" />
+      </div>
+    </form>
+  );
 };
